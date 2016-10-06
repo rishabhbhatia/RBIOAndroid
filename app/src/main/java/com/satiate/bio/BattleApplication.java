45792a295d1c3b90dcf2663dc3af3c22d59950ba
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-10-06 23:47:05 +0530

    New application file, http network interceptor

---
+++ app/src/main/java/com/satiate/bio/BattleApplication.java
@@ -0,0 +1,17 @@
+package com.satiate.bio;
+
+import android.app.Application;
+
+/**
+ * Created by Rishabh Bhatia on 10/6/2016.
+ */
+
+public class BattleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
